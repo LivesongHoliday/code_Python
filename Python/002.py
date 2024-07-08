9f38c667dee0998afe80de17e7788e66e9f29582
LivesongHoliday <ljylivesong@gmail.com>
2024-07-08 14:33:23 +0800

    002

--- Python/002.py
+++ Python/002.py
@@ -124,9 +124,20 @@ name = input('亲，请输入你的名字：')
 age = input('亲，请输入你的年龄：')
 print(f'大家好，我是{name}！我今年{age}岁了！')
 
-# 
+# input() 函数返回的是 str 类型
 num1 = input('请输入第一个数字：')
 num2 = input('请输入第二个数字：')
 sum = num1 + num2
 print(f'sum = {sum}')    # sum = 1020
 
+# 方式一: 相加前转换
+num1 = input('请输入第一个数字：')
+num2 = input('请输入第二个数字：')
+sum = int(num1) + int(num2)
+print(f'sum = {sum}')    # sum = 30
+
+# 方式二: 直接转换
+num1 = int(input('请输入第一个数字：'))
+num2 = int(input('请输入第二个数字：'))
+sum = num1 + num2
+print(f'sum = {sum}')    # sum = 30
