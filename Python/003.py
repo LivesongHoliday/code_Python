849170a3661006f7a317ed5a5c52b86cf02238c1
LivesongHoliday <ljylivesong@gmail.com>
2024-07-09 10:50:38 +0800

    003

--- Python/003.py
+++ Python/003.py
@@ -41,4 +41,43 @@ num1 = 1
 num1 += 1
 print(f'num1 = {num1}')      # 2
 
-# 
+# num2 = 10
+# num2 = num2 - 5
+# print(f'num2 = {num2}')    # 5
+
+num2 = 10
+num2 -= 5
+print(f'num2 = {num2}')      # 5
+
+
+# 逗号运算符: 多个表达式用逗号分隔, 并返回最后一个表达式的值。
+# 原理: 元组解包
+
+# num1 = 10
+# num2 = 20
+# num3 = 30
+# num1, num2, num3 = 10, 20, 30
+
+# num1, num2, num3 = 10, 20, 30, 40, 50
+# ValueError: too many values to unpack (expected 3) 
+
+num1, num2, num3 = 10, 20, 30
+print(f'num1 = {num1}, num2 = {num2}, num3 = {num3}')    # num1 = 10, num2 = 20, num3 = 30
+
+'''
+比较运算符: 对变量或表达式的结果进行大小比较, 其结果是一个布尔值, 即True或False。
+<  小于
+<= 小于等于
+>  大于
+>= 大于等于
+== 等于
+!= 不等于
+'''
+
+num1 = 10
+num2 = 20
+result1 = num1 == num2
+print(f'result1 = {result1}')    # False
+
+result2 = num1 != num2
+print(f'result2 = {result2}')    # True
